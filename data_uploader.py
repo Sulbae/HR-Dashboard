4852f16b8e4f173097b106f2bc810e6b971d4ceb
bangsuwan <angsulwan@gmail.com>
2026-03-25 20:11:33 +0700

    update

--- data_uploader.py
+++ data_uploader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-df = pd.read_csv('')
+df = pd.read_csv('https://raw.githubusercontent.com/Sulbae/HR-Dashboard/refs/heads/main/employee_data.csv')
 
 URL = "postgresql://postgres:9c3UpYRDtrSN3gvJ@db.npxskcigiuiwqdowjouc.supabase.co:5432/postgres"
 
