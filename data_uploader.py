e7abd793bc4255c7ea86c4c4f9518af2a5cd0d67
bangsuwan <angsulwan@gmail.com>
2026-03-25 20:25:35 +0700

    update

--- data_uploader.py
+++ data_uploader.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-df = pd.read_csv('https://raw.githubusercontent.com/Sulbae/HR-Dashboard/refs/heads/main/employee_data.csv')
+CSV_URL = 'https://raw.githubusercontent.com/Sulbae/HR-Dashboard/refs/heads/main/employee_data.csv'
 
-URL = "postgresql://postgres:9c3UpYRDtrSN3gvJ@db.npxskcigiuiwqdowjouc.supabase.co:5432/postgres"
+df = pd.read_csv(CSV_URL)
 
-engine = create_engine(URL)
-df.to_sql('orders', con=engine, if_exists='replace', index=False)
+DATABASE_URL = "postgresql://postgres:9c3UpYRDtrSN3gvJ@db.npxskcigiuiwqdowjouc.supabase.co:5432/postgres"
+
+engine = create_engine(DATABASE_URL)
+df.to_sql('employees', con=engine, if_exists='replace', index=False)
